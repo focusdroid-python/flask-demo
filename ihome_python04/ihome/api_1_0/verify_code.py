85f4c463e3b5fbfe5dd478d897265854da068162
focusdroid-python <focusdroid@126.com>
2020-05-30 19:13:58 +0800

    添加图片验证码和失败的response_code代码

--- ihome_python04/ihome/api_1_0/verify_code.py
+++ ihome_python04/ihome/api_1_0/verify_code.py
@@ -1,10 +1,41 @@
 # coding:utf-8
 
 from . import api
+from ihome.utils.captcha.captcha import captcha
+from ihome import redis_store
+from ihome import constants
+from flask import current_app, jsonify, make_response
+from ihome.utils.response_code import RET
 
-def get_image_code():
+# GET 127.0.0.1/api/v1.0/image_codes/<image_code_id>
+
+@api.route("/image_codes/<image_code_id>")
+def get_image_code(image_code_id):
     """
     获取图片验证码
-    :return: 验证码图片
+    :params image_code_id: 验证码图片
     """
-    pass
+    # 获取参数
+    # 检验参数
+    # 业务处理
+    # 生成验证码图片
+    name, text, image_data = captcha.generate_captcha()
+    # 将验证码真实值与编号保存到redis中, 设置有效期
+    #　redis: 字符串，　列表，　哈希　set
+    #　"key"：xx
+    # "image_data": {"":"","":""} 哈希　hset("image_codes", "id1", "abc")
+    # redis_store.set("image_code_%s" % image_code_id, text)
+    # redis_store.expire("image_code_%s" % image_code_id, constants.IMAGE_CODE_REDIS_EXPIRES)
+    #                     记录名字                          有效期                           记录值
+    try:
+        redis_store.setex("image_dode_%s" % image_code_id, constants.IMAGE_CODE_REDIS_EXPIRES, text)
+    except Exception as e:
+        # 记录日志
+        current_app.logger.error(e)
+        # return jsonify(errno=RET.DBERR, errmsg="save image code id failed")
+        return jsonify(errno=RET.DBERR, errmsg="保存图片验证码失败")
+
+    # 返回数据
+    resp = make_response(image_data)
+    resp.headers["Content-Type"] = "image/jpg"
+    return resp
