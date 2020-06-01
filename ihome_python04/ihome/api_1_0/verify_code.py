eeabf9723b9cc969b2f50890fea6b41b1e0d2386
focusdroid-python <focusdroid@126.com>
2020-06-01 22:21:47 +0800

    解决redis服务问题

--- ihome_python04/ihome/api_1_0/verify_code.py
+++ ihome_python04/ihome/api_1_0/verify_code.py
@@ -7,9 +7,9 @@ from ihome import constants
 from flask import current_app, jsonify, make_response
 from ihome.utils.response_code import RET
 
-# GET 127.0.0.1/api/v1.0/image_codes/<image_code_id>
+# GET 127.0.0.1/api/v1.0/image_code/<image_code_id>
 
-@api.route("/image_codes/<image_code_id>")
+@api.route("/image_code/<image_code_id>")
 def get_image_code(image_code_id):
     """
     获取图片验证码
@@ -28,7 +28,7 @@ def get_image_code(image_code_id):
     # redis_store.expire("image_code_%s" % image_code_id, constants.IMAGE_CODE_REDIS_EXPIRES)
     #                     记录名字                          有效期                           记录值
     try:
-        redis_store.setex("image_dode_%s" % image_code_id, constants.IMAGE_CODE_REDIS_EXPIRES, text)
+        redis_store.setex("image_code_%s" % image_code_id, constants.IMAGE_CODE_REDIS_EXPIRES, text)
     except Exception as e:
         # 记录日志
         current_app.logger.error(e)
