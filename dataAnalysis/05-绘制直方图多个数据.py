5207b6e0eb3743b3577d6b0c12c6f54c1dccb9ce
focusdroid-python <focusdroid@126.com>
2021-04-04 22:28:49 +0800

    feat(add):多条数据只用直方图表示

---
+++ dataAnalysis/05-绘制直方图多个数据.py
@@ -0,0 +1,5 @@
+from matplotlib import pyplot as plt
+from matplotlib import font_manager
+
+_font_manager = font_manager.FontProperties(fname="/usr/share/fonts/opentype/noto/NotoSansCJK-Bold.ttc")
+
